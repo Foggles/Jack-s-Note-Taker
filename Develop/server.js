b67680bef4f17cd98ff41361bd430b322a4531c7
Jack Fogarty <jackfogarty2105@gmail.com>
2021-02-10 18:25:20 +0800

    Pushing current code for instructor

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,16 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const fs = require('fs');
 const express = require('express');
-const http = require('http');
+// const http = require('http');
 const server = express();
 
-
+server.use(express.static('public/assets/css'));
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
+
 server.get('/', (request, response) => response.sendFile(`${__dirname}/public/index.html`));
+server.get('/notes', (request, response) => response.sendFile(`${__dirname}/public/notes.html`));
 
 const noteDatabase = fs.readFile('./db/db.json', (err, data) => {
   if (err) throw err;
@@ -18,32 +20,6 @@ const noteDatabase = fs.readFile('./db/db.json', (err, data) => {
 
 // Specifying the PORT so as to work on Heroku, or on PORT 3000 if not on Heroku
 const PORT = process.env.PORT || 3000;
-
-// const renderHomepage =  (request, response) => {
-//   fs.readFile(`${__dirname}/public/index.html`, (err, data) => {
-//     if (err) throw err;
-//     response.writeHead(200, { 'Content-Type': 'text/html'});
-//     response.end(data);
-//   });
-// }
-
-// const handleRequest = (request, response) => {
-//   const path = request.url;
-
-//   switch (path) {
-//     case '/':
-//         renderHomepage(request, response);
-//       break;
-//     case '/notes':
-//         renderNotes(request, response);
-//         break;
-//     default:
-//       // response.send("Error")
-//       break;
-//   }
-// }
-
-// const server = http.createServer(handleRequest);
 
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
