596e09500a8d6630bf5ab9820bd561fa4afc4e3f
Jack Fogarty <jackfogarty2105@gmail.com>
2021-02-10 12:13:48 +0800

    Restructuring server to server.get

--- Develop/server.js
+++ Develop/server.js
@@ -2,8 +2,10 @@ const mysql = require('mysql');
 const fs = require('fs');
 const express = require('express');
 const http = require('http');
-const { dir } = require('console');
 const app = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
 const noteDatabase = fs.readFile('./db/db.json', (err, data) => {
   if (err) throw err;
@@ -14,14 +16,29 @@ const noteDatabase = fs.readFile('./db/db.json', (err, data) => {
 // Specifying the PORT so as to work on Heroku, or on PORT 3000 if not on Heroku
 const PORT = process.env.PORT || 3000;
 
-const handleRequest = (request, response) => {
-  fs.readFile(`./public/index.html`, (err, data) => {
-    if (err) throw err;
-    
-    response.writeHead(200, { 'Content-Type': 'text/html'});
-    response.end(data);
-  })
-};
+// const renderHomepage =  (request, response) => {
+//   fs.readFile(`${__dirname}/public/index.html`, (err, data) => {
+//     if (err) throw err;
+//     response.writeHead(200, { 'Content-Type': 'text/html'});
+//     response.end(data);
+//   });
+// }
+
+// const handleRequest = (request, response) => {
+//   const path = request.url;
+
+//   switch (path) {
+//     case '/':
+//         renderHomepage(request, response);
+//       break;
+//     case '/notes':
+//         renderNotes(request, response);
+//         break;
+//     default:
+//       // response.send("Error")
+//       break;
+//   }
+// }
 
 const server = http.createServer(handleRequest);
 
