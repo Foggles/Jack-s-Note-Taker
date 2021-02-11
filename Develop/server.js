d27abb710990a57f795e3f999021b6ade0e0a451
Jack Fogarty <jackfogarty2105@gmail.com>
2021-02-11 18:34:56 +0800

    New notes now successfully posted to api / notes

--- Develop/server.js
+++ Develop/server.js
@@ -12,24 +12,35 @@ server.use(express.json());
 server.get('/', (request, response) => response.sendFile(`${__dirname}/public/index.html`));
 server.get('/notes', (request, response) => response.sendFile(`${__dirname}/public/notes.html`));
 
-server.post('/api/notes', (request, response) => {
-  console.log(request);
-  
-  response.json({
-    success: true,
-  })
-});
-
+let notes;
 server.get('/api/notes', (request, response) => {
   // READ JSON, STORE IT INTO VARIABLE, PARSE IT INTO DB.JSON
   fs.readFile(`${__dirname}/db/db.json`, (err, data) => {
     if (err) throw err;
 
-    let db = JSON.parse(data);
-    console.log(db);
-    response.json(db);
+    notes = JSON.parse(data);
+
+    response.json(notes);
   });
 });
+
+server.post('/api/notes', (request, response) => {
+  let noteTitle = request.body.title;
+  let noteText = request.body.text;
+
+  const note = {title: noteTitle, text: noteText};
+  notes.push(note);
+
+  fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(notes), (err) => {
+    if (err) throw err;
+    console.log("Success");
+  });
+  response.json(note);
+});
+
+// server.delete('/api/notes:id', (request, response) => {
+//   console.log(request);
+// })
 
 // Specifying the PORT so as to work on Heroku, or on PORT 3000 if not on Heroku
 const PORT = process.env.PORT || 3000;
